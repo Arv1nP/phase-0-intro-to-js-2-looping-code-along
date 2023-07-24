a5f8cf76a6c39a7f079cb515de32ebedc13a9e12
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-07-24 15:13:27 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+// for (let age = 30; age<=40; age+=2){
+//     console.log(`I'm ${age} years old, happy birthday to me!`);
+//     debugger;
+// }
+
+// const gifts = ['teddy bear','drone','doll' ]
+// function giftWrap(gifts){
+//     for (let i = 0; i<gifts.length; i++){
+//         console.log(`Wrapped ${gifts[i]}, and added a bow!`);
+//         debugger;
+//     }
+//     return gifts;
+// }
+// giftWrap(gifts)
+const event = "birthday";
+const names = ["charlie", "samip","ali"];
+function writeCards(names,event){
+    const thankYouMessage = []
+    for (let i=0; i<names.length; i++){
+        const message = `"Thank you ${names[i]}, for this wonderful ${event} gift!"`;
+        thankYouMessage.push(message);
+        debugger;
+    }
+    return thankYouMessage
+}
+console.log(writeCards(names, event));
