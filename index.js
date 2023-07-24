bfed0a3d20b6a041ed88f01d01510516c9edf305
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-07-24 15:50:15 +1000

    completed again

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-// for (let age = 30; age<=40; age+=2){
-//     console.log(`I'm ${age} years old, happy birthday to me!`);
-//     debugger;
-// }
-
-// const gifts = ['teddy bear','drone','doll' ]
-// function giftWrap(gifts){
-//     for (let i = 0; i<gifts.length; i++){
-//         console.log(`Wrapped ${gifts[i]}, and added a bow!`);
-//         debugger;
-//     }
-//     return gifts;
-// }
-// giftWrap(gifts)
-const event = "birthday";
-const names = ["charlie", "samip","ali"];
+const event = "surprise";
+const names = ["Guadalupe", "Ollie","Aki"];
 function writeCards(names,event){
     const thankYouMessage = []
     for (let i=0; i<names.length; i++){
-        const message = `"Thank you ${names[i]}, for this wonderful ${event} gift!"`;
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
         thankYouMessage.push(message);
         debugger;
     }
     return thankYouMessage
 }
 console.log(writeCards(names, event));
+
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
